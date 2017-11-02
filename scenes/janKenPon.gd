e1f5bb90a3eb71e0bab8a62575a0f9b246f5b90b
Cristian Morataya <cris.morataya@gmail.com>
2017-11-01 23:38:01 -0600

    RC1 of game itch.io version WIP

--- scenes/janKenPon.gd
+++ scenes/janKenPon.gd
@@ -3,17 +3,19 @@ extends Node2D
 onready var rockButton = get_node("hbcJKP/btnRock")
 onready var paperButton = get_node("hbcJKP/btnPaper")
 onready var scissorsButton = get_node("hbcJKP/btnScissors")
-onready var monkeyPlayerSprite = get_node("sprMonkeyPlayer")
-onready var monsterPlayerSprite = get_node("sprMonsterPlayer")
+onready var monkeyPlayerSprite = get_node("gdcMonkey/sprMonkeyPlayer")
+onready var monsterPlayerSprite = get_node("gdcMonster/sprMonsterPlayer")
 
 onready var monkeySelection
 onready var monsterSelection
 
 onready var resultDialog = get_node("resultDialog")
+onready var youWin = false
+
+onready var cheers = get_node("lblWin")
 
 func _ready():
-	var map = [rockButton.get_name(), paperButton.get_name(), scissorsButton.get_name()]
-	print(map)
+	set_process(true)
 
 func _on_btnRock_pressed():
 	monsterElection()
@@ -54,8 +56,10 @@ func winner():
 	elif (monkeySelection == "rock" && monsterSelection == "paper"):
 		return "Monster win"
 	elif (monkeySelection == "rock" && monsterSelection == "scissors"):
+		youWin = true
 		return "You win"
 	if (monkeySelection == "paper" && monsterSelection == "rock"):
+		youWin = true
 		return "You win"
 	elif (monkeySelection == "paper" && monsterSelection == "paper"):
 		return "draw"
@@ -64,6 +68,20 @@ func winner():
 	if (monkeySelection == "scissors" && monsterSelection == "rock"):
 		return "Monster win"
 	elif (monkeySelection == "scissors" && monsterSelection == "paper"):
+		youWin = true
 		return "You win"
 	elif (monkeySelection == "scissors" && monsterSelection == "scissors"):
 		return "draw"
+
+func _on_resultDialog_popup_hide():
+	monkeyPlayerSprite.set_frame(3)
+	monsterPlayerSprite.set_frame(3)
+
+func _process(delta):
+	if (youWin):
+		cheers.show()
+	else:
+		cheers.hide()
+
+func _on_Button_pressed():
+	youWin = false
